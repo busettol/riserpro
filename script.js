f5990c1f9cdf87d7e7176fb246ed65af7f56c7bc
Lucas Busetto <busettol@sheridancollege.ca>
2025-05-30 10:35:30 -0400

    fixed and designed contact form. changed fade in sections

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach(entry => {
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add('visible');
-//       observer.unobserve(entry.target); // optional for one-time animation
-//     }
-//   });
-// }, {
-//   threshold: 0.15
-// });
-
-// // Observe all sections with the fade-in class
-// document.querySelectorAll('.fade-in-section').forEach(section => {
-//   observer.observe(section);
-// });
-
-//updated functino. sections fade in and out. 
+// fade in section function
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
